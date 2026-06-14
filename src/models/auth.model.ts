42a4a2cafdb20654fc7f37993ad4ec76bc645d14
Anthony <anthony77cg@hotmail.com>
2026-06-13 22:13:44 -0500

    feat: colocar campos de id y celular

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -1,8 +1,10 @@
 export interface AuthResponse {
   token: string;
+  id: number;
   nombre: string;
   apellido: string;
   correo: string;
+  celular: string;
   rol: string;
   area: string;
   similitud?: string;
